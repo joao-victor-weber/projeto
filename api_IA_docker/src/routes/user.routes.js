e92083d1a50b16ba10383ae06610affc12513e7b
João Victor Weber <joaovicweber@gmail.com>
2026-05-25 21:20:03 -0300

    limpar diretorio

--- api_IA_docker/src/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-import {Router}from "express";
-import {listUsers, findUserGetId, createUser,deletarUser} from "../controllers/users.controller.js";
-const router=Router();
-router.get("/", listUsers);
-router.get("/:id", findUserGetId);
-router.post("/", createUser);
-router.delete("/:id", deletarUser);
-export default router;
